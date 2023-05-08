162157bf66b028ac3461bdcd2f84cd541db19eea
Luigi Ishii <126471025+luigishii@users.noreply.github.com>
2023-05-08 08:45:14 -0300

    adicionando as funcoes

--- methods.py
+++ methods.py
@@ -5,3 +5,19 @@ def area_of_rectangle(width, height):
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+def soma(a,b):
+    soma = a+b
+    return soma
+
+def subtracao(a,b):
+    subtracao = a-b
+    return subtracao
+
+def multiplicacao(a,b):
+    multiplicacao = a*b
+    return multiplicacao
+
+def divisao(a,b):
+    divisao = a/b
+    return divisao
